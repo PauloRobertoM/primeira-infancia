864a1ca3657e33ff51c2687c903203291e261658
Paulo Alves <front@agencianovi.com.br>
2019-07-26 14:20:45 -0300

    Finalizado

--- components/footer.php
+++ components/footer.php
@@ -1,22 +1,10 @@
-        <footer>
-            <div class="container">
-                <div class="rodape">
-                    <div class="contato">
-                        <p><i class="fa fa-mobile" aria-hidden="true"></i>+ 55 84 9 9962-8000</p>
-                        <p><i class="fa fa-envelope" aria-hidden="true"></i>vaniellyff@hotmail.com.br</p>
-                    </div>
-                    <div class="redes">
-                        <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                        <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                        <a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-            </div>
-        </footer>
+		<footer>
+			<p>Primeira Infância Berçário e Pré-Escola   •    <span>Com carinho a gente constrói o futuro!</span></p>
+		</footer>
+
         <script type='text/javascript' src='assets/js/jquery.js'></script>
         <script type='text/javascript' src='assets/js/bootstrap.min.js'></script>
         <script src="https://use.fontawesome.com/6963733f46.js"></script>
-        <script type='text/javascript' src='assets/js/owl.carousel.min.js'></script>
         <script type='text/javascript' src='assets/js/script.js'></script>
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,104 +1,70 @@
 <?php include 'components/header.php'; ?>
 
-<section class="vitrine">
-	<div class="owl-carousel owl-theme">
-		<a class="item" href="#"><img src="assets/images/banner-vanielly.jpg" alt=""></a>
-		<a class="item" href="#"><img src="assets/images/banner-vanielly.jpg" alt=""></a>
-	</div>
-</section>
-
-<section class="sobre" id="perfil">
-	<div class="container">
+	<section class="geral">
 		<div class="row">
-			<div class="col-md-6">
-				<div class="perfil-foto">
-					<img src="assets/images/perfil-vanielly.jpg" alt="">
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="sobre-texto">
-					<h2>Sobre</h2>
-				<h1><strong>Vanielly</strong></h1>
-				<div class="paragrafo-sobre">
-					<p>Profissional com sólida carreira desenvolvida na área de GESTÃO, vendas e atendimento ao cliente, com destacada experiência em gestão de equipes, treinamento, recrutamento e seleção, nos segmentos de varejo e serviços.</p>
-					<p>Atuação em gestão comercial, administrativa e financeira, com vivência no planejamento e gestão estratégica de vendas, prospecção, estudo e acompanhamento de mercado, desenvolvimento, implementação de ações voltadas ao aumento da visibilidade da empresa e expansão de resultados.</p>
-				</div>
-				<a class="btn-geral" href="#">SAIBA MAIS</a>
-				</div>
-				
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="destaque" id="consultoria">
-	<div class="img-destaque">
-		<img src="assets/images/img-destaque.jpg" alt="">
-	</div>
-	<div class="consultoria-texto">
-		<h2><strong>Consultoria</strong></h2>
-		<p>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only fiverasetons of Lorem Ipsum.</p>
-		<ul>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Diagnóstico Empresarial</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Consultoria Empresarial</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Seleção e capacitação de vendedores</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Negócio de Impacto Social</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Mapeamento de processos</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Treinamentos e palestras</li>
-		</ul>
-		<ul>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Empreendedorismo</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Plano de Marketing</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Pesquisa de satisfação do Cliente</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Cliente oculto</li>
-			<li><i class="fa fa-check" aria-hidden="true"></i>Acompanhamento de planos e metas</li>
-		</ul>
-	</div>
-</section>
-
-<section id="area">
-	<div class="container">
-		<div class="area-cliente">
-			<div class="area-texto">
-				<div class="form-texto">
-					<h3>Área do Cliente</h3>
-					<p>is simply dummy text of the printing and typesetting industry. </p>
-				</div>
-				
-				<div class="form-area">
-					<input type="text" name="">
-					<input type="text" name="">
-					<a class="btn-geral" href="">ENVIAR</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section id="contato">
-	<div class="container">
-		<div class="fale-conosco">
-			<h2><strong>Fale conosco</strong></h2>
-			<form>
-				<div class="form-row">
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Seu nome">
-					</div>
-					<div class="row">
-						<div class="form-group col-md-6">
-							<input type="text" class="form-control" placeholder="E-mail">
-						</div>
-						<div class="form-group col-md-6">
-							<input type="text" class="form-control" placeholder="Telefone">
-						</div>
-					</div>
-					<textarea class="form-control" placeholder="Mensagem" rows="6"></textarea>
-				</div>
-				<a class="btn-geral" href="">ENVIAR</a>
-			</form>
-		</div>
-	</div>
-</section>
-
+			<div class="container">
+				<div class="col-md-6 col-sm-6">
+					<img src="assets/images/logo-primeira-infancia.png" alt="" class="logo">
+					<h1>Estamos muito felizes com sua visita, e gostariamos de retribuir sua gentileza.</h1>
+					<p>Para que possamos fazer novo contato e também avaliarmos nossas condições de atendimento, pedimos que preencha este formulário.</p>
+				</div><!-- md-6 -->
+				<div class="col-md-6 col-sm-6">
+					<form action="">
+						<label for="">Nome da criança</label>
+						<input type="text" class="form-control">
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label for="">Idade</label>
+								<input type="text" class="form-control">
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label for="">Parentesco</label>
+								<input type="text" class="form-control">
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<label for="">Responsável pela visita</label>
+						<input type="text" class="form-control">
+						<label for="">CPF</label>
+						<input type="text" class="form-control">
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label for="">E-mail</label>
+								<input type="text" class="form-control">
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label for="">Telefone</label>
+								<input type="text" class="form-control">
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<label for="">Endereço</label>
+						<input type="text" class="form-control">
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label for="">Bairro</label>
+								<input type="text" class="form-control">
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label for="">Cidade</label>
+								<input type="text" class="form-control">
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<label for="" class="item-sel">Qual o mês de início pretendido?</label>
+						<select name="" id="" class="form-control">
+							<option value="">Item 1</option>
+							<option value="">Item 2</option>
+							<option value="">Item 3</option>
+						</select>
+						<label for="" class="item-sel">Como conheceu a escola?</label>
+						<select name="" id="" class="segundo form-control">
+							<option value="">Item 1</option>
+							<option value="">Item 2</option>
+							<option value="">Item 3</option>
+						</select>
+						<button><img src="assets/images/bg-btn.png" alt=""></button>
+					</form>
+				</div><!-- md-6 -->
+			</div><!-- container -->
+		</div><!-- row -->
+	</section><!-- geral -->
 	
 <?php include 'components/footer.php'; ?>
